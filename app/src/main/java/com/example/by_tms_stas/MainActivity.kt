0dcb01cef7921314ffc39b9021ee2124940fabe1
Stasotto <stasmikulin@gmail.com>
2021-09-22 19:09:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/by_tms_stas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.by_tms_stas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
